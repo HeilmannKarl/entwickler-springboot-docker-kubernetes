8215fec586a3cb33a40def67463009988a9517dc
Karl Heilmann <kh@karl-heilmann.info>
2023-01-20 21:32:33 +0100

    update

--- src/main/java/com/heilmann/entwickler/docker/springboot/app/AppApplication.java
+++ src/main/java/com/heilmann/entwickler/docker/springboot/app/AppApplication.java
@@ -2,12 +2,23 @@ package com.heilmann.entwickler.docker.springboot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
 
 @SpringBootApplication
+@RestController
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    @RequestMapping("/")
+    public String index() throws Exception {
+        InetAddress ip = InetAddress.getLocalHost();
+        return "Hello Container World\nIP: " +ip;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
 }
